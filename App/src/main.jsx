9a427d230951dbd3c28fc6cf02a4b05bb9aea8ba
Velvet-Samayoa <vsamyoaa@miumg.edu.gt>
2023-10-19 14:45:20 -0600

    Creación de carpetas

--- App/src/main.jsx
+++ App/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Login from './assets/componentes/login/login'
+import 'bootstrap/dist/css/bootstrap.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
